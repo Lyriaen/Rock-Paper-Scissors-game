9a6ba4b304fb9fb8e268d013f9295ca58132b353
Anna Mirocha <annamirocha93@gmail.com>
2022-10-28 23:07:48 +0200

    show game choise option and add basic styles for them

--- src/script/steps/stepOne.js
+++ src/script/steps/stepOne.js
@@ -1,7 +1,31 @@
 import { clearMainWindow } from "../utils/functions.js"
 
 export const createStepOneView = (version) => {
+    showFooter()
+    clearMainWindow()
+    const gameVersion = version.name
+    createOptionsElement(version.options)
+}
+
+const createOptionsElement = (options) => {
+    const mainElement = document.querySelector('.main')
+    for (const [option, icon] of Object.entries(options)) {
+        const newOptionElement = createOptionElement(option, icon)
+        mainElement.append(newOptionElement)
+    }
+}
+
+const createOptionElement = (option, icon) => {
+    const optionElement = document.createElement('div')
+    optionElement.classList.add(`option-${option}`, 'option')
+    const iconElement = document.createElement('div')
+    iconElement.classList.add('option-icon')
+    // optionElement.innerText = option
+    optionElement.append(iconElement)
+    return optionElement
+}
+
+const showFooter = () => {
     const footer = document.querySelector('.footer')
     footer.style.display = 'block'
-    clearMainWindow()
 }
--- src/script/utils/versions.js
+++ src/script/utils/versions.js
@@ -1,22 +1,24 @@
 export const basicVersion = {
-    rules: './../../images/image-rules.svg',
+    name: 'basic',
+    rules: 'image-rules.svg',
     options: {
-        rock: './../../images/icon-rock.svg',
-        paper: './../../images/icon-paper.svg',
-        scissors: './../../images/icon-scissors.svg'
+        rock: 'icon-rock.svg',
+        paper: 'icon-paper.svg',
+        scissors: 'icon-scissors.svg'
     },
-    background: './../../images/bg-triangle.svg'
+    background: 'bg-triangle.svg'
 }
 
 
 export const bonusVersion = {
-    rules: './../../images/image-rules-bonus.svg',
+    name: 'bonus',
+    rules: 'image-rules-bonus.svg',
     options: {
-        rock: './../../images/icon-rock.svg',
-        paper: './../../images/icon-paper.svg',
-        scissors: './../../images/icon-scissors.svg',
-        lizard: './../../images/icon-lizard.svg',
-        spock: './../../images/icon-spock.svg'
+        rock: 'icon-rock.svg',
+        paper: 'icon-paper.svg',
+        scissors: 'icon-scissors.svg',
+        lizard: 'icon-lizard.svg',
+        spock: 'icon-spock.svg'
     },
-    background: './../../images/bg-pentagon.svg'
+    background: 'bg-pentagon.svg'
 }
