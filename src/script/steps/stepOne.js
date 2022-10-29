daaca81ca87167294af8a748c12b37377f2c1421
Anna Mirocha <annamirocha93@gmail.com>
2022-10-29 22:31:55 +0200

    improve appearance of step one

--- src/script/steps/stepOne.js
+++ src/script/steps/stepOne.js
@@ -4,19 +4,22 @@ export const createStepOneView = (version) => {
     showFooter()
     clearMainWindow()
     const gameVersion = version.name
-    createOptionsElement(version.options, version.backgroundImage)
+    createOptionsElement(version.name, version.options, version.backgroundImage)
 }
 
-const createOptionsElement = (options, backgroundImageName) => {
+const createOptionsElement = (version, options, backgroundImageName) => {
     const mainElement = document.querySelector('.main')
     const optionsContainerElement = document.createElement('section')
     optionsContainerElement.classList.add('main_optionsContainer')
+
     const backgroundImage = document.createElement('img')
     backgroundImage.src = `./src/images/${backgroundImageName}`
-    backgroundImage.classList.add('main_optionsContainer_backgroundImage')
+    backgroundImage.classList.add('main_optionsContainer_backgroundImage', classDependsOnVersion(version))
+
     optionsContainerElement.append(backgroundImage)
     for (const [option, position] of Object.entries(options)) {
         const newOptionElement = createOptionElement(option, position)
+        newOptionElement.classList.add(classDependsOnVersion(version))
         optionsContainerElement.append(newOptionElement)
     }
     mainElement.append(optionsContainerElement)
@@ -25,17 +28,31 @@ const createOptionsElement = (options, backgroundImageName) => {
 const createOptionElement = (option, position) => {
     const optionElement = document.createElement('div')
     optionElement.classList.add(`option-${option}`, 'option')
-    const iconElement = document.createElement('div')
-    iconElement.classList.add('option-icon')
-    // optionElement.innerText = option
-    console.log(position.top)
     optionElement.style.top = position.top + '%'
     optionElement.style.left = position.left + '%'
-    optionElement.append(iconElement)
+
+    const iconContainerElement = document.createElement('div')
+    iconContainerElement.classList.add('iconContainer')
+    optionElement.append(iconContainerElement)
+
+    const iconElement = document.createElement('img')
+    iconElement.src = `./src/images/icon-${option}.svg`
+    iconElement.classList.add('option-icon')
+    iconContainerElement.append(iconElement)
+
     return optionElement
 }
 
 const showFooter = () => {
     const footer = document.querySelector('.footer')
     footer.style.display = 'block'
+}
+
+const classDependsOnVersion = (version) => {
+    if (version === 'basic') {
+        return 'basic-version'
+    }
+    if (version === 'bonus') {
+        return 'bonus-version'
+    }
 }
